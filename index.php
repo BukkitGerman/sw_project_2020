59e00cb24babfaf9f2342b35736f554df7995bf2
Justin Preuß <preussjustin@outlook.de>
2020-05-14 10:24:59 +0200

    modified index.php (add Navigation Skelet)

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Blog</title>
+</head>
+<nav class="menu">
+	<ol>
+        <li class="crumb"><a href="/">Home</a></li>
+        <li class="crumb"><a href="">Posts</a></li>
+        <li class="crumb"><a href="">Profile</a></li>
+        <li class="crumb"><a href="impressum.php">Impressum</a></li>
+        <li class="crumb" id="login"><a href="login.php"></a></li> 
+    </ol>
+</nav>
+<body>
+# Newest Blog Post
+</body>
+</html>
