2f4a58673fbd00ae3e0b7576938ec3789dcfd426
Justin Preuß <preussjustin@outlook.de>
2020-05-14 10:41:41 +0200

    modified index.php file

--- index.php
+++ index.php
@@ -8,11 +8,22 @@
         <li class="crumb"><a href="/">Home</a></li>
         <li class="crumb"><a href="">Posts</a></li>
         <li class="crumb"><a href="">Profile</a></li>
-        <li class="crumb"><a href="impressum.php">Impressum</a></li>
-        <li class="crumb" id="login"><a href="login.php"></a></li> 
+        <li class="crumb" id="login"><a href="login.php">Login</a></li> 
     </ol>
 </nav>
 <body>
-# Newest Blog Post
+<div>
+	<div class="post">
+		<!-- 
+		Newest Blog Post
+		Neusten Post aus Datenbank auslesen und Anzeigen.
+		-->
+	</div>
+</div>
 </body>
+<footer>
+	Author: Justin Preuß
+	Copyright © 2020 Justin Preuß
+	<a href="impressum.php">Impressum</a>
+</footer>
 </html>
