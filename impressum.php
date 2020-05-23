01a9caed130052d880373733c6d7dbc97ba5c94b
Justin Preuß <preussjustin@outlook.de>
2020-05-23 14:07:41 +0200

    fix navbar

--- impressum.php
+++ impressum.php
@@ -6,10 +6,20 @@
 <nav class="menu">
 	<ol>
         <li class="crumb"><a href="/">Home</a></li>
-        <li class="crumb"><a href="posts.php">Posts</a></li>
-        <li class="crumb"><a href="profile.php">Profile</a></li>
-        <li class="crumb" id="login"><a href="login.php">Login</a></li> 
-    </ol>
+		<li class="crumb"><a href="posts.php">Posts</a></li>
+		<?php 
+        	if(isset($_SESSION['userid'])){
+        		echo '<li class="crumb active"><a href="profile.php?user='.$_SESSION["userid"].'">Profile</a></li>';
+        	}
+        ?>
+		<?php
+        if(!isset($_SESSION['userid'])){
+        echo '<li class="crumb" id="login"><a href="login.php">Login</a></li>';
+    	}else{
+    	echo '<li class="crumb" id="logout"><a href="logout.php">Logout</a></li>';
+    	}
+        ?>
+	</ol>
 </nav>
 <body>
 	<div class="middle box">
